aa20ef498307ea09fab9fde0d6a1ae776e44e591
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-03-27 15:00:36 -0300

    passing the model to the server

--- server/titanic/auth.py
+++ server/titanic/auth.py
@@ -1,10 +1,11 @@
 import functools
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
+from . import create_app
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+app = create_app()
+
+bp = Blueprint('auth', __name__, url_prefix='/')
 
 @bp.route('/', methods=('GET', 'POST'))
 def register():
