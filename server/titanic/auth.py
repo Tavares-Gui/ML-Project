16a57b28b319f024df4f4d0091df1eb613f9ded8
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-03-28 15:47:54 -0300

    AAAAAAAAAAAAAAAAAA

--- server/titanic/auth.py
+++ server/titanic/auth.py
@@ -1,42 +1,52 @@
-import pickle
 import functools
-from joblib import dump, load
+import numpy as np
+import pandas as pd
 
-from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from joblib import dump, load
+from flask import (Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for)
 
 bp = Blueprint('auth', __name__, url_prefix='/')
 
-@bp.route('/submit', methods=('POST',))
+@bp.route('/', methods=['GET']) 
+def index(): 
+    return render_template('index.html') 
+
+@bp.route('/submit', methods=['POST'])
 def submit():
-    age = request.form.get('age')
-    fare = request.form.get('fare')
-    pClass = request.form.get('pClass')
-    gender = request.form.get('gender')
-    embarked = request.form.get('embarked')
+    Pclass = request.form.get('pClass')
+    Sex = request.form.get('gender')
+    Age = request.form.get('age')
+    SibSp = request.form.get('sibSp')
+    Parch = request.form.get('parch')
+    Fare = request.form.get('fare')
+    Embarked = request.form.get('embarked')
     
+    Pclass = int(Pclass)
+
+    Age = float(Age)
+    
+    SibSp = int(SibSp)
+    
+    Parch = int(Parch)
+    
+    Fare = float(Fare)
+    
+    data = [[Pclass, Sex, Age, SibSp, Parch, Fare, Embarked]]
+
     LRmodel = load('../models/LogisticRegression.pkl')
     ENmodel = load('../models/ElasticNet.pkl')
-    
-    if gender == 'male':
-        gender = 1
-    else:
-        gender = 0
-        
-    if embarked == 'Q':
-        embarked = 1
-    elif embarked == 'S':
-        embarked = 2
-    else:
-        embarked = 3
-        
-    age = int(age)
-    
-    data = [[pClass, gender, age, embarked, fare]]
-    
-    Ypred = model.predict(data)
-    
-    return render_template('result.html', prediction=Ypred[0])
 
-@bp.route('/', methods=('GET', 'POST'))
-def register():
-    return render_template('index.html')
+    df = pd.DataFrame(data)
+
+    predict = LRmodel.predict(df)
+
+    print(predict)
+
+    pred = str(predict[0])
+
+    print(pred)
+
+    return pred
+
+if __name__ == '__main__': 
+    app.run()
