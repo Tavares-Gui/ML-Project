1b7a8b581d71273e406fc0af4e3e60ddf0e8dacb
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-03-28 16:38:18 -0300

    CABO

--- server/titanic/auth.py
+++ server/titanic/auth.py
@@ -13,32 +13,44 @@ def index():
 
 @bp.route('/submit', methods=['POST'])
 def submit():
-    Pclass = request.form.get('pClass')
-    Sex = request.form.get('gender')
-    Age = request.form.get('age')
-    SibSp = request.form.get('sibSp')
-    Parch = request.form.get('parch')
-    Fare = request.form.get('fare')
-    Embarked = request.form.get('embarked')
+    # Pclass = request.form.get('pClass')
+    # Sex = request.form.get('gender')
+    # Age = request.form.get('age')
+    # SibSp = request.form.get('sibSp')
+    # Parch = request.form.get('parch')
+    # Fare = request.form.get('fare')
+    # Embarked = request.form.get('embarked')
     
-    Pclass = int(Pclass)
+    # if Sex == 'male':
+    #     Sex = 0
+    # if Sex == 'female':
+    #     Sex = 1
+    
+    # if Embarked == 'Q':
+    #     Embarked = 0
+    # if Embarked == 'S':
+    #     Embarked = 1
+    # if Embarked == 'C':
+    #     Embarked = 2
+    
+    data = request.form
 
-    Age = float(Age)
+    personInfos = {
+        "Pclass": [data["pClass"]],
+        "Sex": {'male': 0, 'female': 1 }[data["gender"]],
+        "Age": [data["age"]],
+        "SibSp": [data["sibSp"]],
+        "Parch": [data["parch"]],
+        "Fare": [data["fare"]],
+        "Embarked": {'Q': 0, 'S': 1, 'C': 2}[data["embarked"]]
+    }
     
-    SibSp = int(SibSp)
-    
-    Parch = int(Parch)
-    
-    Fare = float(Fare)
-    
-    data = [[Pclass, Sex, Age, SibSp, Parch, Fare, Embarked]]
-
+    data = pd.DataFrame(personInfos)
+        
     LRmodel = load('../models/LogisticRegression.pkl')
     ENmodel = load('../models/ElasticNet.pkl')
 
-    df = pd.DataFrame(data)
-
-    predict = LRmodel.predict(df)
+    predict = LRmodel.predict(data)
 
     print(predict)
 
