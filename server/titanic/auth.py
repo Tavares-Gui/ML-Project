0985e06140711fdfc43b7f69d0aad64c99ccc848
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-04-01 15:32:19 -0300

    end

--- server/titanic/auth.py
+++ server/titanic/auth.py
@@ -13,26 +13,6 @@ def index():
 
 @bp.route('/submit', methods=['POST'])
 def submit():
-    # Pclass = request.form.get('pClass')
-    # Sex = request.form.get('gender')
-    # Age = request.form.get('age')
-    # SibSp = request.form.get('sibSp')
-    # Parch = request.form.get('parch')
-    # Fare = request.form.get('fare')
-    # Embarked = request.form.get('embarked')
-    
-    # if Sex == 'male':
-    #     Sex = 0
-    # if Sex == 'female':
-    #     Sex = 1
-    
-    # if Embarked == 'Q':
-    #     Embarked = 0
-    # if Embarked == 'S':
-    #     Embarked = 1
-    # if Embarked == 'C':
-    #     Embarked = 2
-    
     data = request.form
 
     personInfos = {
